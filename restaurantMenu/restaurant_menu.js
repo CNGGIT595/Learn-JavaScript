29c46b218a60b253939f33e809ef63048f156cad
CNGGIT595 <chong.xiao@ato.gov.au>
2025-11-11 18:40:05 -0500

    updated restaurant_menu files

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $30', 'Pasta - $25', 'Burger - $22', 'Salmon - $28'];
+const dessertMenu = ['Cake - $20', 'Ice Cream - $15', 'Pudding - $15', 'Fruit Salad - $18'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
