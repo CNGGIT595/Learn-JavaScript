a9f926f1b795ee1179889023d55c49c763fd6f94
CNGGIT595 <chong.xiao@ato.gov.au>
2025-11-11 00:41:00 -0500

    update calculate_Aarea files

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,13 @@ let area = length * width;
 
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+function groceryTracker () {
+  let a = parseFloat(document.getElementById('grocery1').value);
+  let b = parseFloat(document.getElementById('grocery2').value);
+  let c = parseFloat(document.getElementById('grocery3').value);
+  let totalAmount = calculateGrocery(a,b,c)
+  document.getElementById('TotalAmount').innerText = `The total amount is: ${totalAmount}`;
+}
+function calculateGrocery(a, b, c) {
+  return totalAmount = a + b + c ;
+}
